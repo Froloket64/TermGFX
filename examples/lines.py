7e7ce9f4b9fc14d0d7025de563821dadf7044662
Froloket64 <ket.frol2006@gmail.com>
2022-05-09 15:48:04 +0300

    Modified example to better show off engine's features

--- examples/lines.py
+++ examples/lines.py
@@ -1,13 +1,18 @@
 from TermGFX.engine import Canvas
 from TermGFX.shapes import Line
 
-
 window_size = (20, 22)
 
 canvas = Canvas(window_size)
 
-line = Line((0, 0), (5, 8), "█", algorithm="dda")
+lines = []
 
-line.draw(canvas)
+lines.append(Line((0, 0), (5, 8), "█"))
+lines.append(Line((0, 0), (0, 8), "█"))
+lines.append(Line((0, 8), (5, 8), "█"))
+lines.append(Line((10, 10), (1, 12), "█"))
 
-canvas.draw(clear=False)
+for line in lines:
+    line.draw(canvas)
+
+canvas.draw()
